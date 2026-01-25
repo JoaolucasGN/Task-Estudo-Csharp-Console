c21b6f76b87d596be6e29e4abed64292089b8ba8
JoaolucasGN <joaolucascpvv@gmail.com>
2026-01-24 23:32:14 -0300

    Add files via upload

---
+++ Csharp9/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+
+    if (i == 7)
+    {
+        break;
+    }
+}
+*/
+/*
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+for (int i = names.Lenght - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+*/
+/*
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+for (int i = 0; i < names.Lenght; i++)
+{
+    Console.WriteLine(names[i]);
+}
+*/
+/*
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
+*/
+/*
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+for (int i = 0; i < names.Lenght; i++)
+{
+    if (names == "David")
+    {
+        names[i] == "Sammy";
+    }
+    foreach (var novonames in names)
+    {
+        Console.WriteLine(novonames);
+    }
+}
+*/
+
+
